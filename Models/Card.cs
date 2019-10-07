ff3a9a53c27da70c744c6862949c9b371779abf6
Tanguy Bouckaert <tanguy.bouckaert@student.hogent.be>
2019-10-07 14:21:24 +0200

    add class Card

---
+++ Models/Card.cs
@@ -0,0 +1,14 @@
+﻿namespace BlackJackGame.Models
+{
+    public class Card
+    {
+        public FaceValue _faceValue { get; }
+        public Suit _suit { get; }
+
+        public Card(Suit suit, FaceValue faceValue)
+        {
+            _faceValue = faceValue;
+            _suit = suit;
+        }
+    }
+}
