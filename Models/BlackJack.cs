9849f6a8367d4ba2c9191492d14db253a7f07e43
Tanguy Bouckaert <tanguy.bouckaert@student.hogent.be>
2019-10-14 18:32:21 +0200

    added class BlackJack

---
+++ Models/BlackJack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJackGame.Models
+{
+    public class BlackJack
+    {
+        bool FaceDown;
+        bool FaceUp;
+
+        public GameState GameState { get; set; }
+
+        public void AddCardToHand(Hand hand, bool faceUp)
+        {
+
+        }
+
+        public void AdjustGameState([GameState? gameState = null])
+        {
+
+        }
+
+        public BlackJack()
+        {
+
+        }
+
+        public BlackJack(Deck deck)
+        {
+
+        }
+
+        public void Deal()
+        {
+
+        }
+
+        public string GameSummary()
+        {
+
+        }
+    }
+}
