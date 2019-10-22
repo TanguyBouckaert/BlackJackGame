e655c990c1a639a9db5fe0788f01df49ad1bbdef
Tanguy Bouckaert <tanguy.bouckaert@student.hogent.be>
2019-10-22 11:10:11 +0200

    - compileerd - text is niet in juiste weergegeven

--- Models/BlackJack.cs
+++ Models/BlackJack.cs
@@ -3,61 +3,83 @@ namespace BlackJackGame.Models
 {
     public class BlackJack
     {
-        bool FaceDown;
-        bool FaceUp;
-
-        #region Properties
-        public gameState GameState { get; set; }
-        public Hand PlayerHand { get; set; }
-        public Hand DealerHand { get; set; }
-        public Deck _deck { get; set; }
+        #region Fields
+        public const bool FaceDown = false;
+        public const bool FaceUp = true;
+        private readonly Deck _deck;
         #endregion
 
-        public void AddCardToHand(Hand hand, bool faceUp)
-        {
+        #region Properties 
+        //Setters moeten private zijn!!!!
+        public gameState GameState { get; private set; }
+        public Hand PlayerHand { get; private set; }
+        public Hand DealerHand { get; private set; }
+        #endregion
 
+
+        #region Constructors
+        public BlackJack() : this(new Deck()) { }
+
+        public BlackJack(Deck deck)
+        {//vergeten methode deal toe te voegen!!!!
+            _deck = deck;
+            PlayerHand = new Hand();
+            DealerHand = new Hand();
+            Deal();
+        }
+        #endregion
+
+        #region Methodes
+
+        public void AddCardToHand(Hand hand, bool faceUp)
+        {//methode niet ingevuld!!!!!
+            BlackJackCard card = _deck.Draw();
+            if (faceUp)
+                card.TurnCard();
+            hand.AddCard(card);
         }
 
         private void AdjustGameState(gameState? GameState = null)
-        {
+        {// deel van gamesummary moet eigenlijk hier staan
+            /*
             if (this.GameState == gameState.PlayerPlays)
             {
                 int totaal = PlayerHand.CalculateValue();
                 //totaal >= 21 ? this.GameState = new gameState(): this.GameState;
             }
+            */
+
+            if (GameState.HasValue)
+                GameState = GameState.Value;
+            if (GameState == gameState.PlayerPlays && PlayerHand.Value >= 21)
+                PassToDealer();
+            if (GameState == gameState.DealerPlays && (PlayerHand.Value > 21 || DealerHand.Value >= 21 || DealerHand.Value >= PlayerHand.Value))
+                GameState = gameState.GameOver;
         }
-
-        public BlackJack()
-        {
-            _deck = new Deck();
-            PlayerHand = new Hand();
-            DealerHand = new Hand();
-            
-        }
-
-        public BlackJack(Deck deck)
-        {
-            _deck = deck;
-
-            PlayerHand = new Hand();
-            DealerHand = new Hand();
-            
-        }
-
-        #region Methodes
         public void Deal()
         {
-            PlayerHand.AddCard(_deck.Draw()); 
+            /*
+            PlayerHand.AddCard(_deck.Draw());
             PlayerHand.AddCard(_deck.Draw());
             PlayerHand.TurnAllCardsFaceUp();
 
-            PlayerHand.CalculateValue().Equals(21) ? Console.WriteLine($"{GameSummary()}"):AdjustGameState(gameState.PlayerPlays);
+            !!!!! Verkeerde methode gebruikt om kaart toe te voegen !!!!!
+
+            PlayerHand.CalculateValue().Equals(21) ? Console.WriteLine($"{GameSummary()}") : AdjustGameState(gameState.PlayerPlays);
             DealerHand.AddCard(_deck.Draw()); //niet zeker of face up of down
             DealerHand.AddCard(_deck.Draw()); //niet zeker of face up of down
+            */
+
+            AddCardToHand(DealerHand, FaceUp);
+            AddCardToHand(DealerHand, FaceDown);
+            AddCardToHand(PlayerHand, FaceUp);
+            AddCardToHand(PlayerHand, FaceUp);
+            AdjustGameState(gameState.PlayerPlays);
         }
 
         public string GameSummary()
-        {
+        {//deel zit al in methode adjustgamestate, kon ook opgelost worden met enkel if's
+            /*
             if (this.GameState == gameState.GameOver)
             {
                 switch (PlayerHand.CalculateValue().Equals(21) || DealerHand.CalculateValue().Equals(21))
@@ -92,25 +114,51 @@ namespace BlackJackGame.Models
             {
                 return null;
             }
+            */
+
+            if (GameState != gameState.GameOver)
+                return null;
+            if (PlayerHand.Value > 21)
+                return "Player burned, dealer wins";
+            if (PlayerHand.Value == 21 && PlayerHand.NrOfCards == 2 && DealerHand.Value != 21)
+                return "BLACKJACK";
+            if (PlayerHand.Value == DealerHand.Value)
+                return "Equal, dealer wins";
+            if (DealerHand.Value > 21)
+                return "Dealer burned, player wins";
+            if (DealerHand.Value > PlayerHand.Value)
+                return "Dealer wins";
+            return "Player wins";
         }
 
         public void GivePlayerAnotherCard()
-        {
+        {//weer slechte methode gebruikt !!!!!
+            /*
             if (this.GameState == gameState.PlayerPlays)
             {
                 PlayerHand.AddCard(_deck.Draw());
-            }
+            }*/
+
+            if (GameState != gameState.PlayerPlays)
+                throw new InvalidOperationException("You cannot take a card now...");
+            AddCardToHand(PlayerHand, FaceUp);
+            AdjustGameState();
         }
 
         public void LetDealerFinalize()
-        {
-            AdjustGameState(gameState.DealerPlays);
+        {//while luske vergeten en methode addcardtohand !!!!!!
+            while(GameState == gameState.DealerPlays)
+            {
+                AddCardToHand(DealerHand, FaceUp);
+                AdjustGameState();
+            }
         }
 
         public void PassToDealer()
-        {
+        {//methode letdealerfinalize vergeten!!!!!!
             DealerHand.TurnAllCardsFaceUp();
             AdjustGameState(gameState.DealerPlays);
+            LetDealerFinalize();
         }
         #endregion
     }
